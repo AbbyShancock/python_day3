90e73179b91cac0f53d51d7ea04e5ad9aa022dec
Saucedo <ASaucedo44@cps.k12.il.us>
2024-08-30 15:07:47 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,94 @@
+#create a print function that print out "Hello, World!"
+# out the string "Hello, World!"
+print("Hello, World!")
+# ask the user for the day of the week
+
+# day = input("What day of the week is it?" )
+# print("The day of the week is " + day + "!!!")
+#concatenation is when you add two strings together
+# using a plus sign
+
+# movies_this_week = input("What movies are you watching this week?")
+# print("I am watching " + movies_this_week + " this week")
+
+# mood = input("How are you feeling today?")
+# print("I am feeling " + mood + " today")
+
+# data types for variables in python
+
+# strings - text
+name = "John"
+year = "year"
+ # this is a string datatype
+#Whenever it is wrapped in quotation marks it is a string, could be double or single
+
+# integers - whole numbers
+year = 2024
+yearFourFromNow = 2028
+subtract = yearFourFromNow - year
+print(subtract)
+ # this is an interger data type
+# does not need to be wrapped in quotes
+
+# floats - decimal numbers
+pricebigmac = 3.99 # this is a float datatype
+pricedoublepounder = 4.99
+totalprice = pricebigmac + pricedoublepounder
+print(totalprice)
+
+# booleans - true or false
+israining = False # this is a boolean datatype
+print(israining)
+
+# lists - a collection of items
+groceries = ["apples", "bananas", "carrots"]
+print(groceries)
+
+#challenge #1
+#you and your family are going to a movie and dinner
+#you need a list of movies to choose from
+#the time of the movie
+#the time of the dinner
+# the name of the movie
+#how many people are going
+#print the entire output out in one sentence.
+
+address = "5698 w 65th Place"
+
+
+time_of_movie = "6PM"
+
+
+time_of_dinner = "8:30PM"
+
+
+name_of_movie = "DeadpoolvsWolverine"
+
+
+price_of_movie = 13.99
+
+
+price_of_dinner = 59.99
+
+
+ismovieplayinginevening = True
+
+
+how_many_people = input("How many people are going?" )
+
+
+print("The address of the movie is " + address +
+       " and at " + time_of_movie + ". Dinner starts at " +
+         time_of_dinner + " after we see " + name_of_movie +
+           ". The price of the movie is " + price_of_movie + " and the price of the dinner is " +
+             price_of_dinner + ". The fact that the movie plays in the evening is " +
+               ismovieplayinginevening + " and " + how_many_people + " are going.")
+
+
+
+
+
+
+
+
+
---
+++ python_learning/python_basics/lesson_1.py
@@ -0,0 +1,41 @@
+#mkdir means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create file
+#rm means remove a file
+#pound sign means comment
+# print("Hello, World!")
+# print("How are you?")
+# print("bazinga")
+# print("age \t 25")
+# \t means tab
+# press control ? to comment out multiple lines
+
+name = input("What is your name? ")
+print("Hello, " + name)
+
+# ask for age
+#print out age
+#ask for their place of birth
+#print out place of birth
+#ask for their address
+#print out address
+#ask what school they went to
+#print out school
+
+age = input("What is your age? ")
+print("You're " +  age  + " 5years old")
+
+place_of_birth = input("What is your place of birth?")
+print("You were born in " + place_of_birth)
+
+address = input("What is your address?")
+print("Your address is "  + address)
+
+schoolyougoto = input("What school did you go to?")
+print("You went to the school called "  + schoolyougoto)
+
+#create a sentence or print statement that includes all of the information you have collected
+
+print("You're name is " + name + ", you were born in " + place_of_birth + ", you're address is  " + address ", and you go to " + schoolyougoto + " school")
